f53b31458b3b77f4a00d65e9eb77233f26487983
Bryan Paronto <bparonto@gmail.com>
2018-01-27 22:48:14 -0600

    adds todo comment

--- api/src/router.js
+++ api/src/router.js
@@ -3,6 +3,7 @@ import postsController from './controllers/PostsController'
 
 const router = express.Router();
 
+// TODO: write users controller
 // router.get('/users', usersController.list);
 // router.get('/users/:id', usersController.show);
 // router.post('/users', usersController.add);
