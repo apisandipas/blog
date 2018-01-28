d71e64aa539b26251c852a2be70a99969af7957a
Bryan Paronto <bparonto@gmail.com>
2018-01-28 15:20:34 -0600

    adjusted posts seeder

--- api/db/seeds/posts.js
+++ api/db/seeds/posts.js
@@ -1,14 +1,19 @@
 const faker = require('faker')
-
+const range = require('lodash.range')
+const map = require('lodash.map')
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('posts').del()
     .then(function () {
       // Inserts seed entries
-      return knex('posts').insert([
-        {id: 1, title: faker.lorem.words(), body: faker.lorem.paragraphs()},
-        {id: 2, title: faker.lorem.words(), body: faker.lorem.paragraph()},
-        {id: 3, title: faker.lorem.words(), body: faker.lorem.paragraphs()}
-      ]);
+      // 
+      const posts = map(range(1, 100, 1), (i) => {
+        return  {
+          id: i, 
+          title: faker.lorem.words(), 
+          body: faker.lorem.paragraphs()
+        }
+      })
+      return knex('posts').insert(posts);
     });
 };
