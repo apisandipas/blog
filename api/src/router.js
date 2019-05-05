36b0e8261e2d896837e40be3a4d9c2376f5ec396
Bryan Paronto <bparonto@gmail.com>
2019-05-04 19:56:05 -0500

    updating packages

--- api/src/router.js
+++ api/src/router.js
@@ -1,4 +1,4 @@
-import express  from 'express'
+import express from 'express'
 import passport from 'passport'
 import requireRole from './middleware/require-role'
 import postsController from './controllers/PostsController'
@@ -10,37 +10,29 @@ const router = express.Router()
 const requireLogin = passport.authenticate('local', { session: false })
 const requireToken = passport.authenticate('jwt', { session: false })
 
-router.post('/login',
-  requireLogin,
-  authController.login
-)
+router.post('/login', requireLogin, authController.login)
 router.get('/logout', authController.logout)
 router.post('/register', authController.register)
 router.post('/forgot-password', authController.forgotPassword)
 router.post('/reset-password', authController.resetPassword)
 router.post('/username-available', authController.isUsernameAvailable)
 
-router.get('/users',
-  requireToken,
-  requireRole('ADMIN'),
-  usersController.index
-)
-router.get('/users/:id',
+router.get('/users', requireToken, requireRole('ADMIN'), usersController.index)
+router.get(
+  '/users/:id',
   requireToken,
   requireRole('ADMIN'),
   usersController.show
 )
-router.post('/users',
-  requireToken,
-  requireRole('ADMIN'),
-  usersController.add
-)
-router.put('/users/:id',
+router.post('/users', requireToken, requireRole('ADMIN'), usersController.add)
+router.put(
+  '/users/:id',
   requireToken,
   requireRole('ADMIN'),
   usersController.update
 )
-router.delete('/users/:id',
+router.delete(
+  '/users/:id',
   requireToken,
   requireRole('ADMIN'),
   usersController.delete
@@ -48,21 +40,18 @@ router.delete('/users/:id',
 
 router.get('/posts', postsController.index)
 router.get('/posts/:slug', postsController.show)
-router.post('/posts',
-  requireToken,
-  requireRole('ADMIN'),
-  postsController.add
-)
-router.put('/posts/:slug',
+router.post('/posts', requireToken, requireRole('ADMIN'), postsController.add)
+router.put(
+  '/posts/:slug',
   requireToken,
   requireRole('ADMIN'),
   postsController.update
 )
-router.delete('/posts/:slug',
+router.delete(
+  '/posts/:slug',
   requireToken,
   requireRole('ADMIN'),
   postsController.delete
 )
 
 export default router
-
